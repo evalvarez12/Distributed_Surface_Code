49181cde51f50eee2f250a53ad02b754abb0395a
Eduardo Villasenor <evalvarez12@gmail.com>
2018-04-24 11:29:57 +0200

    data pg

--- decomposition/results_protocols.py
+++ decomposition/results_protocols.py
@@ -32,7 +32,7 @@ np.random.seed(4567890)
 ps = 0.003
 pm = 0.003
 pg = 0.003
-a0 = 30.0
+a0 = 5.0
 a1 = 1/30.
 eta = 1/100.
 theta = .63
@@ -41,7 +41,7 @@ theta = .63
 # a0 = 1/2.
 # eta = 1/200
 # Protocol name to save state
-protocol_name = "thres_a0_parallel"
+protocol_name = "thres_eta_parallel"
 
 def env_error_rate(t, a):
     # Function to calculate the error to the enviroment for step of stabilizers
@@ -53,7 +53,7 @@ def env_error_rate(t, a):
 
 # Number of iterations for a average
 iterations = 2000
-ignore_number = int(iterations/10)
+ignore_number = int(iterations/100*5.)
 
 # Initialize objects and define references
 bell_ref = qt.bell_state('00') * qt.bell_state('00').dag()
@@ -79,14 +79,17 @@ targets = list(range(stab_size))
 
 # Start from 6000.0
 # for s in [0]:
-for a0 in [500.0, 600.0, 700.0, 800.0, 900.0, 1000.0, 1100.0, 1200.0, 1300.0, 1400.0, 1500.0]:
+# for a0 in [500.0, 600.0, 700.0, 800.0, 900.0, 1000.0, 1100.0, 1200.0, 1300.0, 1400.0, 1500.0]:
 # for eta in [0.0055, 0.0050, 0.0045, 0.0040, 0.0035, 0.0030]:
 # for eta in [0.0100, 0.0095, 0.0090, 0.0085, 0.0080, 0.0075, 0.0070, 0.0065, 0.0060, 0.0055, 0.0050]:
+for pg in [0.0031, 0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.0040, 0.0041]:
+    ps = pg
+    pm = pg
     FIDELITY = []
     TIMES = []
-    print("------> Var=", a0)
+    print("------> Var=", pg)
     print("EPL")
-    ghz = protocols.ghz4_epl_parallel(ps, pm, pg, eta, a0, a1, theta)
+    ghz = protocols.ghz4_epl(ps, pm, pg, eta, a0, a1, theta)
     # Get average number of steps
     fidelity = []
     times = []
