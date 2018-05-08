0512a89a1d893c0354b1abce9678d3e941074163
Eduardo Villaseñor <evalvarez12@gmail.com>
2018-05-09 00:44:43 +0200

    DATA CHI ETA

--- decomposition/result_extract_chi.py
+++ decomposition/result_extract_chi.py
@@ -22,21 +22,22 @@ eta = 1/100.
 theta = .63
 
 # GHZ info
-ghz_size = 2
+ghz_size = 3
 stab_size = 4
-protocol = "thres_pg"
-protocol_chi = "thres_pg_paired"
+protocol = "thres_eta"
+protocol_chi = "thres_eta_paired"
 
 extra = False
 ignore_percent = 5
 
 TIME = []
 
-# for eta in [0.01, 0.0095, 0.0090, 0.0085, 0.0080, 0.0075, 0.0070, 0.0065, 0.0060, 0.0055, 0.0050, 0.0045]:
+# for eta in [0.01, 0.0095, 0.0090, 0.0085, 0.0080, 0.0075, 0.0070, 0.0065, 0.0060, 0.0055, 0.0050]:
+for eta in [0.0070, 0.0065, 0.0060, 0.0055, 0.0050, 0.0045, 0.0040, 0.0035, 0.0030, 0.0025, 0.0020, 0.0015, 0.0010, 0.0005]:
 # for a0 in [2000.0, 3500.0, 4000.0, 4500.0, 5000.0, 5500.0, 6000.0]:
 # for a0 in [10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0]:
 # for pg in [0.0031, 0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.0040, 0.0041]:
-for pg in [0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.0040, 0.0041, 0.0042, 0.0043, 0.0044, 0.0045]:
+# for pg in [0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.0040, 0.0041, 0.0042, 0.0043, 0.0044, 0.0045]:
     ps = pg
     pm = pg
     # Load GHZ state files
@@ -90,6 +91,8 @@ for pg in [0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.004
         model.separate_basis_parity()
         stab = stabilizer.Stabilizer(ps=ps, pm=pm, pg=pg)
 
+        ghz = stab.twirl_ghz(ghz)
+
         # Choi state for noise noise modeling
         choi = model._choi_state_ket(stab_size)
         choi = choi * choi.dag()
@@ -100,7 +103,6 @@ for pg in [0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.004
             extra_name = "_extra"
         protocol_chi2 =  protocol_chi + extra_name
 
-        ghz = stab.twirl_ghz(ghz)
         p_res, rhos = stab.measure_ghz_stabilizer(choi, ghz, targets, parity)
 
         # Set channel output and make chi matrix
--- decomposition/results_protocols.py
+++ decomposition/results_protocols.py
@@ -41,7 +41,7 @@ theta = .63
 # a0 = 1/2.
 # eta = 1/200
 # Protocol name to save state
-protocol_name = "thres_eta_bk"
+protocol_name = "thres_eta"
 
 def env_error_rate(t, a):
     # Function to calculate the error to the enviroment for step of stabilizers
@@ -52,7 +52,7 @@ def env_error_rate(t, a):
 
 
 # Number of iterations for a average
-iterations = 100
+iterations = 2000
 ignore_number = int(iterations/100*5.)
 
 # Initialize objects and define references
@@ -61,7 +61,7 @@ bell_ref2 = qt.bell_state('01') * qt.bell_state('01').dag()
 ghz4_ref = qt.ghz_state(4) * qt.ghz_state(4).dag()
 ghz3_ref = qt.ghz_state(3) * qt.ghz_state(3).dag()
 
-rho_ref = ghz4_ref
+rho_ref = ghz3_ref
 
 # Stabilizer and error modeling stuff
 stab_size = 4
@@ -80,7 +80,7 @@ targets = list(range(stab_size))
 # Start from 6000.0
 # for s in [0]:
 # for a0 in [5.0]:
-for eta in [0.0055, 0.0050]:
+for eta in [0.0009, 0.0008, 0.0007, 0.0006]:
 # for eta in [0.0100, 0.0095, 0.0090, 0.0085, 0.0080, 0.0075, 0.0070, 0.0065, 0.0060, 0.0055, 0.0050]:
 # for pg in [0.0031, 0.0032, 0.0033, 0.0034, 0.0035, 0.0036, 0.0037, 0.0038, 0.0039, 0.0041]:
 # for pg in [0.0040]:
@@ -89,8 +89,8 @@ for eta in [0.0055, 0.0050]:
     FIDELITY = []
     TIMES = []
     print("------> Var=", eta)
-    print("EPL BK")
-    ghz = protocols.ghz4_bk(ps, pm, pg, eta, a0, a1, theta)
+    print("EPL")
+    ghz = protocols.ghz3_epl(ps, pm, pg, eta, a0, a1, theta)
     # Get average number of steps
     fidelity = []
     times = []
