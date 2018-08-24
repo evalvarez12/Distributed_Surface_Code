b5cee8e2af66c0b678e44df435bc24821a12e162
Ben Criger <bcriger@gmail.com>
2018-08-24 13:10:46 +0200

    CHANGED INDENTATION TO SPACES IN ANALYSIS.PY

--- analysis.py
+++ analysis.py
@@ -6,13 +6,20 @@ learn more about what's under the hood.
 """
 
 def properties(obj):
-	"""
-	Returns a set of names which correspond to properties of an object:
-	these names can be used to access parts of the object, but not
-	methods, class methods, etc.
+    """
+    Returns a set of names which correspond to properties of an object:
+    these names can be used to access parts of the object, but not
+    methods, class methods, etc.
 
-	For example, if you used this function on a Point object with
-	internal `x`, `y`, and `z`, you'd get back `{'x', 'y', 'z'}`, but 
-	nothing about `__add__`.
-	"""
-	return set(dir(obj)) - set(dir(type(obj)))
+    For example, if you used this function on a Point object with
+    internal `x`, `y`, and `z`, you'd get back `{'x', 'y', 'z'}`, but 
+    nothing about `__add__`.
+    """
+    return set(dir(obj)) - set(dir(type(obj)))
+
+def internals(obj):
+    """
+    For convenience, gets the values associated with 'properties' and 
+    returns them in a dict.
+    """
+    return {_: obj.__dict__[_] for _ in properties(obj)}
